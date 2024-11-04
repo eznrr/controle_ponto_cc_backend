ecc74d1742e1f893ff8f76e9b4bf5997ab825725
eznrr <eznr527@sempreceub.com>
2024-11-04 10:38:30 -0300

    Comecando o backend

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express();
+
+//app.METODO('rota/caminho', (req, res) => {})
+app.get('/users', (req, res) => {
+    res.send("Usuarios: ")
+});
+
+app.post('/users', (req, res) => {
+    res.send("Rota users usando post")
+});
+
+app.get('/user/:id', (req, res) => {
+    res.send(`O parametro é ${req.params.id}`);
+});
+
+app.post('/user/:id1-:id2', (req, res) => {
+    res.send(req.params);
+});
+
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor web ouvindo porta ${PORT}`)
+})
